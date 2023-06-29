0da902313808979d4fb342d76a53ce2ee590f50b
Akash Goswami <goswamiakash1@outlook.com>
2023-06-29 14:52:36 +0530

    modified

--- TextUtill/urls.py
+++ TextUtill/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from  . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "TextUtils"
 admin.site.site_title = "TextUtils Admin Portal"
@@ -26,3 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('textu.urls'))    
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
