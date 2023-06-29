e9daaac4cc2acdc3af845d67ec68a60623f337ce
Akash Goswami <goswamiakash1@outlook.com>
2023-06-30 01:11:46 +0530

    com

--- TextUtill/settings.py
+++ TextUtill/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zam756(t@$395-b9z=l_@y=+_e8ut^a7ualrvz6=48+2bz&k%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
